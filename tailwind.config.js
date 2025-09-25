44556fea9f640c2328c582ac992c3592c15ca3ba
unknown <ekeleolu1@gmail.com>
2025-09-25 17:06:38 +0100

    commit now

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   darkMode: "class", // important!
   content: ["./src/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-inter)", "ui-sans-serif", "system-ui"],
+      },
+    },
+  },
+
   plugins: [],
 };
