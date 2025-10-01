cd031e2f8c400a144b7d80b4c7fdd46404b8ad32
unknown <ekeleolu1@gmail.com>
2025-10-01 17:03:49 +0100

    commit with backend url

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,5 @@
 import type { Metadata } from "next";
-// import localFont from "next/font/local";
 import "./globals.css";
-
-// ✅ Local Inter font
-// const inter = localFont({
-//   src: [
-//     {
-//       path: "../public/fonts/Inter/Inter-Regular.woff2",
-//       weight: "400",
-//       style: "normal",
-//     },
-//     {
-//       path: "../public/fonts/Inter/Inter-Medium.woff2",
-//       weight: "500",
-//       style: "normal",
-//     },
-//     {
-//       path: "../public/fonts/Inter/Inter-SemiBold.woff2",
-//       weight: "600",
-//       style: "normal",
-//     },
-//     {
-//       path: "../public/fonts/Inter/Inter-Bold.woff2",
-//       weight: "700",
-//       style: "normal",
-//     },
-//   ],
-//   variable: "--font-inter",
-//   display: "swap",
-// });
 
 export const metadata: Metadata = {
   title: "Eke e-commerce",
--- lib/api.ts
+++ lib/api.ts
@@ -1,12 +1,16 @@
 import axios from "axios";
 
-const api =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:6040"
-    : "https://ekemazon-b-git-main-ekeles-projects.vercel.app";
+// const api =
+//   process.env.NODE_ENV === "development"
+//     ? "http://localhost:6040"
+//     : process.env.NODE_ENV === "production"
+//     ? "https://ekemazon-b.vercel.app/"
+//     : "http://localhost:6040";
 
+const local = "http://localhost:6040";
+const server = "https://ekemazon-b.vercel.app";
 export const axiosInstance = axios.create({
-  baseURL: api,
+  baseURL: server,
   headers: {
     "Content-Type": "application/json",
   },
